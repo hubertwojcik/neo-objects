a8f46ef37006f5b450a9b0a3b72fa5f5d72e77bc
Hubert Wójcik <hwojcikk@gmail.com>
2023-12-20 18:13:18 +0100

    chore: export screen size

--- src/shared/utils/responsibility.ts
+++ src/shared/utils/responsibility.ts
@@ -1,13 +1,24 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, PixelRatio } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
 const guidlineBaseWidth = 390;
 const guidlineBaseHeight = 844;
 
-export const horizontalScale = (size: number) =>
-  (width / guidlineBaseWidth) * size;
-export const verticalScale = (size: number) =>
-  (height / guidlineBaseHeight) * size;
+const widthRatio = width / guidlineBaseWidth;
+const heightRatio = height / guidlineBaseHeight;
+
+export const horizontalScale = (size: number) => widthRatio * size;
+
+export const verticalScale = (size: number) => heightRatio * size;
+
 export const moderateScale = (size: number, factor = 0.5) =>
   size + (horizontalScale(size) - size) * factor;
+
+export const normalize = (size: number, forHeight?: boolean) => {
+  const newSize = size * (forHeight ? heightRatio : widthRatio);
+  return Math.round(PixelRatio.roundToNearestPixel(newSize));
+};
+
+export const SCREEN_WIDTH = width;
+export const SCREEN_HEIGHT = height;
