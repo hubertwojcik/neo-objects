9e81f3cb6b78fd4f53de86aed18e2abed33ecfab
Hubert Wójcik <hwojcikk@gmail.com>
2024-01-02 12:50:06 +0100

    fix: fix black color

--- src/shared/constants/colors.ts
+++ src/shared/constants/colors.ts
@@ -1,7 +1,7 @@
 type Color = 'accent' | 'white' | 'black' | 'background';
 
 export const colors: Record<Color, string> = {
-  black: '2c2c2c',
+  black: '#2c2c2c',
   white: '#fff',
   accent: '#FFBB3B',
   background: '#F8F4E9',
