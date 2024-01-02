6487b2dca2dbb414bb54037974f78d6fb8398c28
Hubert Wójcik <hwojcikk@gmail.com>
2024-01-02 13:03:25 +0100

    refactor: screen refactor

--- src/screens/neo-object-details/neo-objects-details-screen.tsx
+++ src/screens/neo-object-details/neo-objects-details-screen.tsx
@@ -1,146 +1,117 @@
+import { useGetNeoObjectById } from '@/api/neo-objects';
 import {
-  View,
-  Text,
-  useWindowDimensions,
-  ScrollView,
-  Pressable,
-} from 'react-native';
-import React from 'react';
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+  Meteorite,
+  NeoDetailsHeader,
+  NeoObjectDetailsList,
+} from '@/components';
 import type { NeoObjectsStackParamList } from '@/navigation/neo-objects-navigator';
-import {
-  Canvas,
-  CornerPathEffect,
-  DiscretePathEffect,
-  rect,
-  Circle,
-  vec,
-  Group,
-  Skia,
-  Rect,
-} from '@shopify/react-native-skia';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors } from '@/shared/utils';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import Animated, {
-  Extrapolate,
+  Extrapolation,
   interpolate,
+  interpolateColor,
+  useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
-  withTiming,
 } from 'react-native-reanimated';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type NeoObjectDetailsScreenProps = NativeStackScreenProps<
   NeoObjectsStackParamList,
   'NeoObjectDetails'
 >;
 
-const METEORITE_HEIGHT = 150;
-const METEORITE_WIDTH = 300;
+const HEADER_MAX_HEIGHT = 300;
+const HEADER_MIN_HEIGHT = 20;
 
-export const NeoObjectDetails = ({
-  navigation,
-}: NeoObjectDetailsScreenProps) => {
-  const rotateX = useSharedValue(0);
-  const rotateY = useSharedValue(0);
+const SCROLL_MAX_VALUE = 250;
+const SCROLL_MIN_VALUE = 0;
 
-  const gesture = Gesture.Pan()
-    .onBegin((event) => {
-      rotateX.value = withTiming(
-        interpolate(
-          event.y,
-          [0, METEORITE_HEIGHT],
-          [10, -10],
-          Extrapolate.CLAMP,
-        ),
-      );
-      rotateY.value = withTiming(
-        interpolate(
-          event.x,
-          [0, METEORITE_WIDTH],
-          [-10, 10],
-          Extrapolate.CLAMP,
-        ),
-      );
-    })
-    .onUpdate((event) => {
-      rotateX.value = interpolate(
-        event.y,
-        [0, METEORITE_HEIGHT],
-        [10, -10],
-        Extrapolate.CLAMP,
-      );
-      rotateY.value = interpolate(
-        event.x,
-        [0, METEORITE_WIDTH],
-        [-10, 10],
-        Extrapolate.CLAMP,
-      );
-    })
-    .onFinalize(() => {
-      rotateX.value = withTiming(0);
-      rotateY.value = withTiming(0);
-    });
-  const meteoriteAnimatedStyles = useAnimatedStyle(() => {
-    const rotateXvalue = `${rotateX.value}deg`;
-    const rotateYvalue = `${rotateY.value}deg`;
+const BORDER_TOP_RADIUS = 50;
 
-    return {
-      transform: [
-        {
-          perspective: 300,
-        },
-        { rotateX: rotateXvalue },
-        { rotateY: rotateYvalue },
-      ],
-    };
-  }, []);
-  //
-  // /
-  const { width } = useWindowDimensions();
+export const NeoObjectDetails = ({ route }: NeoObjectDetailsScreenProps) => {
+  const { id, objectName } = route.params;
+
+  const { data } = useGetNeoObjectById(id);
+
   const insets = useSafeAreaInsets();
 
-  const meteorite = rect(50, 50, METEORITE_WIDTH, METEORITE_HEIGHT);
+  const neoDetails = data || null;
 
-  const clip = Skia.Path.Make();
-  clip.addOval(meteorite);
+  const scrollY = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const headerStyles = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
+        scrollY.value,
+        [SCROLL_MIN_VALUE, SCROLL_MAX_VALUE],
+        [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+        Extrapolation.CLAMP,
+      ),
+    };
+  });
+
+  const listStyles = useAnimatedStyle(() => {
+    return {
+      borderTopLeftRadius: interpolate(
+        scrollY.value,
+        [SCROLL_MIN_VALUE, SCROLL_MAX_VALUE],
+        [BORDER_TOP_RADIUS, 0],
+        Extrapolation.CLAMP,
+      ),
+      borderTopRightRadius: interpolate(
+        scrollY.value,
+        [SCROLL_MIN_VALUE, SCROLL_MAX_VALUE],
+        [BORDER_TOP_RADIUS, 0],
+        Extrapolation.CLAMP,
+      ),
+    };
+  });
+
+  const screenStyles = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        scrollY.value,
+        [0, 150],
+        [colors.white, colors.black],
+      ),
+    };
+  });
+
   return (
-    <ScrollView style={{ backgroundColor: 'white', paddingTop: insets.top }}>
-      <View style={{ backgroundColor: 'red' }}>
-        <Pressable
-          style={{
-            position: 'absolute',
-            left: 0,
-            zIndex: 20,
-            backgroundColor: 'green',
-          }}
-          onPress={() => {
-            navigation.goBack();
-          }}
-        >
-          <Text>WROC</Text>
-        </Pressable>
-        <Text style={{ textAlign: 'center' }}>NAME OF OBJECT</Text>
-      </View>
-      <GestureDetector gesture={gesture}>
-        <Animated.View style={meteoriteAnimatedStyles}>
-          <Canvas style={{ width, height: 300 }}>
-            <Group clip={clip}>
-              <Rect
-                rect={rect(50, 50, 300, METEORITE_HEIGHT)}
-                color="darkgray"
-                origin={meteorite}
-              >
-                <CornerPathEffect r={60} />
-                <DiscretePathEffect length={40} deviation={40} />
-              </Rect>
-              <Circle r={30} c={vec(300, 50)} color="grey" />
-              <Circle r={20} c={vec(100, 100)} color="grey" />
-              <Circle r={15} c={vec(200, 150)} color="grey" />
-              <Circle r={30} c={vec(310, 170)} color="grey" />
-            </Group>
-          </Canvas>
-        </Animated.View>
-      </GestureDetector>
-    </ScrollView>
+    <Animated.View
+      style={[
+        { flex: 1, backgroundColor: colors.white, paddingTop: insets.top },
+        screenStyles,
+      ]}
+    >
+      <NeoDetailsHeader title={objectName} scrollValue={scrollY} />
+      <Animated.View style={headerStyles}>
+        <Meteorite animatedValue={scrollY} />
+      </Animated.View>
+      <Animated.ScrollView
+        scrollEventThrottle={5}
+        bounces={false}
+        style={[styles.listWrapper, listStyles]}
+        onScroll={scrollHandler}
+      >
+        <NeoObjectDetailsList details={neoDetails} />
+      </Animated.ScrollView>
+    </Animated.View>
   );
 };
+
+const styles = StyleSheet.create({
+  listWrapper: {
+    backgroundColor: colors.black,
+    borderTopLeftRadius: BORDER_TOP_RADIUS,
+    borderTopRightRadius: BORDER_TOP_RADIUS,
+  },
+});
